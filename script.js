4ba5bb36d5a8c9fa8585ceeb79b8d41a9168a8d7
musicbender <musicbender@gmail.com>
2015-12-02 07:51:12 -0800

    added group classes

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
     
+    //Click Start Button
     $('.start-btn').click(function(){
+        $('.start').fadeOut(750);
         $('.start-btn').fadeOut(750, function(){
             $('.progress').fadeIn(750);
-            $('.audio-section').fadeIn(750);
-            $('.answers').fadeIn(750, function(){
+            $('.audio-1').fadeIn(750);
+            $('.answers').fadeIn(750);
+            $('.group-1').fadeIn(750, function(){
                 $('.progress').fadeTo(750, 1);
             });
         });
-
     });
+    
+    
     
     
     
