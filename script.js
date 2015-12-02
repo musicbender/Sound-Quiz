5f291b3155bfd66d6a996301cb3081ab55509d95
musicbender <musicbender@gmail.com>
2015-12-02 12:35:20 -0800

    worked out first transition into group 2 FROM GROUP 1

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
     
+    var quizNum = 1;
+    
     //Click Start Button
     $('.start-btn').click(function(){
         $('.start').fadeOut(750);
         $('.start-btn').fadeOut(750, function(){
             $('.progress').fadeIn(750);
+            $('.audio-section').fadeIn(750);
             $('.audio-1').fadeIn(750);
             $('.answers').fadeIn(750);
             $('.group-1').fadeIn(750, function(){
@@ -13,10 +16,25 @@ $(document).ready(function(){
         });
     });
     
+    //Click on answerbox to reveal right answer and move to the next
+    
+    $('.answer-box').click(function(){
+        console.log(quizNum);
+        
+        $('.group-' + quizNum + ' .correct').addClass('correct-reveal');
+        $('.group-' + quizNum + ' .wrong').fadeTo(1000, 0);
+        $('.group-' + quizNum).delay(1000).fadeOut(1000).delay(1000, function(){
+            quizNum++;
+            $('.group-' + quizNum).fadeIn(1000);
+            console.log(quizNum);
+        });
+    });
+    
+    
+    //Hover effect for Answer-Box
     $('.answer-box').mouseenter(function(){
         $(this).addClass('answer-box-hover');
     });
-    
     $('.answer-box').mouseleave(function(){
         $(this).removeClass('answer-box-hover');
     });
